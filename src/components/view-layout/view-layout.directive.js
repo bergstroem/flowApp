31741ff2fbc3a7540365c0b4a551520a30624ed0
Mattias Bergström <bergstroem1@gmail.com>
2014-12-06 18:18:58 +0100

    Remove newline

--- src/components/view-layout/view-layout.directive.js
+++ src/components/view-layout/view-layout.directive.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('flowApp')
-.directive('viewLayout', function () {
+angular.module('flowApp').directive('viewLayout', function () {
   return {
     restrict: 'E',
     transclude: true,
