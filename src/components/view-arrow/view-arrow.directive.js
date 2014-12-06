adf96b61605c4a25ef3d2bb417259c3e7f4c00a7
Elin Nilsson <elin.nilsson30@gmail.com>
2014-12-06 18:38:13 +0100

    kluhuik

--- src/components/view-arrow/view-arrow.directive.js
+++ src/components/view-arrow/view-arrow.directive.js
@@ -4,6 +4,9 @@ angular.module('flowApp')
 .directive('viewArrow', function () {
   return {
     restrict: 'E',
+    scope {
+    	
+    },
     templateUrl: '/components/view-arrow/view-arrow.directive.html'
   };
 });
